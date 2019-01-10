284996a34a02315ed807dd45efbc260094d783b5
G. D. McBain <gdmcbain@protonmail.com>
2019-01-10 16:33:22 +1100

    plotted transects of load and deflexion #1

--- 02_poisson_membrane/st02_poisson_membrane.py
+++ 02_poisson_membrane/st02_poisson_membrane.py
@@ -1,3 +1,4 @@
+from matplotlib.pyplot import subplots
 import numpy as np
 
 from skfem import *
@@ -7,16 +8,20 @@ import dmsh
 
 
 mesh = MeshTri(*map(np.transpose,
-                    dmsh.generate(dmsh.Circle([0., 0.], 1.), 1/64)))
+                    dmsh.generate(dmsh.Circle([0., 0.], 1.), np.sqrt(2)/64)))
 basis = InteriorBasis(mesh, ElementTriP2())
 
 boundary = basis.get_dofs().all()
 
+beta = 8.
+R0 = .6
+
+def load_f(x):
+    return 4 / np.exp(beta**2 * (x[0]**2 + (x[1] - R0)**2))
+
 @linear_form
 def load(v, dv, w):
-    beta = 8.
-    R0 = .6
-    return 4 * v / np.exp(beta**2 * (w.x[0]**2 + (w.x[1] - R0)**2))
+    return v * load_f(w.x)
 
 A = asm(laplace, basis)
 b = asm(load, basis)
@@ -25,3 +30,16 @@ w = np.zeros_like(b)
 w[basis.complement_dofs(boundary)] = solve(*condense(A, b, D=boundary))
 
 mesh.save('w.xdmf', w)
+
+y = np.linspace(-1, 1, 103)[1:-1]
+yy = np.vstack([np.zeros_like(y), y])
+
+fig, ax = subplots()
+ax.plot(load_f(yy), y, marker='x', label='load')
+factor = 50
+ax.plot(factor * basis.interpolator(w)(yy), y,
+        marker='o', label=f'deflexion × {factor}')
+ax.set_xlabel('deflexion')
+ax.set_ylabel('y/a')
+ax.legend()
+fig.savefig('curves.png')
