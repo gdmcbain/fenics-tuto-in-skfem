98f336dc2e03ceee73cb3057fb1fddca14f63f5f
G. D. McBain <gdmcbain@protonmail.com>
2019-01-30 11:30:19 +1100

    root, fixes #2

--- 05_poisson_nonlinear/README.md
+++ 05_poisson_nonlinear/README.md
@@ -2,10 +2,10 @@ Translation into scikit-fem from FEniCS of
 [ft05_poisson_nonlinear.py](https://fenicsproject.org/pub/tutorial/python/vol1/ft05_poisson_nonlinear.py)
 as discussed in [‘A nonlinear Poisson equation’](https://fenicsproject.org/pub/tutorial/html/._ftut1007.html#ftut1:gallery:nonlinearpoisson).
 
-The big new feature here is the use of [SymPy](https://sympy.org) to manufacture the right-hand side from an assumed solution.
+The big new features here are:
+* the use of [SymPy](https://sympy.org) to manufacture the right-hand side from an assumed solution
+* the use of [scipy.optimize.root](https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.root.html#scipy.optimize.root) as the nonlinear solver
 
-![initial.png](poisson_nonlinear/initial.png)(*a*) ![solution.png](poisson_nonlinear/solution.png)(*b*)
+![exact.png](poisson_nonlinear/exact.png)(*a*) ![solution.png](poisson_nonlinear/solution.png)(*b*)
 
-*Figure:—* (*a*) initial condition and (*b*) exact solution, both should be equal to, 1 + _x_ + 2 _y_
-
-*Figure:—* Initial condition, 1 + _x_ + 2 _y_
+*Figure:—* (*a*) exact 1 + _x_ + 2 _y_ and (*b*) numerical solutions
--- 05_poisson_nonlinear/st05_poisson_nonlinear.py
+++ 05_poisson_nonlinear/st05_poisson_nonlinear.py
@@ -1,58 +1,84 @@
 from pathlib import Path
 
+import numpy as np
+from scipy.optimize import root
+
 from sympy import symbols
 from sympy.vector import CoordSys3D, gradient, divergence
 from sympy.utilities.lambdify import lambdify
 
 from skfem import (MeshTri, InteriorBasis, ElementTriP1,
-                   linear_form, asm, solve, condense)
+                   bilinear_form, linear_form, asm, solve, condense)
 from skfem.models.poisson import laplace
 
+
 output_dir = Path('poisson_nonlinear')
+
 try:
     output_dir.mkdir()
 except FileExistsError:
     pass
 
-R = CoordSys3D('R')
-
-u = 1 + R.x + 2*R.y
 
 def q(u):
     """Return nonlinear coefficient"""
     return 1 + u**2
 
-f = -divergence(q(u) * gradient(u))
 
-print('u:', u)
-print('f:', f)
+R = CoordSys3D('R')
+
+
+def apply(f, coords):
+    x, y = symbols('x y')
+    return lambdify((x, y), f.subs({R.x: x, R.y: y}))(*coords)
+
+
+u_exact = 1 + R.x + 2*R.y                  # exact solution
+f = -divergence(q(u_exact) * gradient(u_exact))  # manufactured RHS 
 
 mesh = MeshTri()
 mesh.refine(3)
 
 V = InteriorBasis(mesh, ElementTriP1())
 
-def funcify(f):
-    x, y = symbols('x y')
-    return lambdify((x, y), f.subs({R.x: x, R.y: y}))
-
-u_D = funcify(u)
-
-u = u_D(*mesh.p)
-mesh.plot(u).get_figure().savefig(str(output_dir.joinpath('initial.png')))
-
-f_f = funcify(f)
-
-@linear_form
-def load(v, dv, w):
-    return v * f_f(*w.x)
-
-a = asm(laplace, V)
-b = asm(load, V)
-
 boundary = V.get_dofs().all()
 interior = V.complement_dofs(boundary)
 
-u[interior] = solve(*condense(a, b, u, D=boundary))
 
-mesh.plot(u).get_figure().savefig(str(output_dir.joinpath('solution.png')))
+@linear_form
+def load(v, dv, w):
+    return v * apply(f, w.x)
+
+
+b = asm(load, V)
+
+
+@bilinear_form
+def diffusion_form(u, du, v, dv, w):
+    return sum(dv * (q(w.w) * du))
+
+
+def diffusion_matrix(u):
+    return asm(diffusion_form, V, w=V.interpolate(u))
+
+dirichlet = apply(u_exact, mesh.p)    # P1 nodal interpolation
+mesh.plot(u).get_figure().savefig(str(output_dir.joinpath('exact.png')))
+
+
+def residual(u):
+    r = b - diffusion_matrix(u) @ u
+    r[boundary] = 0.
+    return r
+
+
+u = np.zeros(V.N)
+u[boundary] = dirichlet[boundary]
+result = root(residual, u, method='krylov')
+
+if result.success:
+    u = result.x
+    print('Success: residual =', np.linalg.norm(residual(u), np.inf))
+    print(' or nodally Linf:', np.linalg.norm(u - dirichlet, np.inf))
+    mesh.plot(u).get_figure().savefig(str(output_dir.joinpath('solution.png')))
+else:
+    print(result)
